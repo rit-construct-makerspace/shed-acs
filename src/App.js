d88cb03d4a97b856c16041cb2887e32cd57fd347
el6179 <el6179@rit.edu>
2023-03-05 17:41:43 -0500

    Testing onoff library

--- src/App.js
+++ src/App.js
@@ -55,6 +55,20 @@ const App = () => {
   //Machine Maintenance time
   const [machineTime, setMachineTime] = useState(MACHINE_TIME_FRAME)
 
+  //gpio
+  const Gpio = require('onoff').Gpio;
+  const led = new Gpio('17', 'out');
+// set BCM 4 pin as 'output'
+const ledOut = new Gpio( '4', 'out' );
+
+// current LED state
+let isLedOn = false;
+
+// run a infinite interval
+setInterval( () => {
+  ledOut.writeSync( isLedOn ? 0 : 1 ); // provide 1 or 0 
+  isLedOn = !isLedOn; // toggle state
+}, 3000 ); // 3s
   /*
   This function is called every time the uid-textbox is updated
   */
@@ -129,6 +143,7 @@ const App = () => {
     setOutput("Current User: " + validUid)
     setUser(validUid)
     setInUse("Machine in Use")
+    led.writeSync(1);
   }
 
   /*
@@ -140,6 +155,7 @@ const App = () => {
     setOutput("")
     setInUse("")
     setUserTime(USER_TIME_FRAME)
+    led.writeSync(0);
   }
 
   function resetRequest(){
